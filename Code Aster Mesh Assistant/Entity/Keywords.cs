65c4f510456f974cadd08721181c4b7007825a93
ibobey <ibrahim-tunc-02@hotmail.com>
2025-08-27 14:17:02 +0300

    Keywords inserted

---
+++ Code Aster Mesh Assistant/Entity/Keywords.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Aster_Mesh_Assistant.Entity
+{
+    internal class Keywords
+    {
+    }
+}
