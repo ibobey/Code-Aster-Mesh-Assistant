5624dc7067e17b1c1f7cafb86ac7d60f44229f38
ibobey <ibrahim-tunc-02@hotmail.com>
2025-08-27 13:54:12 +0300

    first commit

---
+++ Code Aster Mesh Assistant/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Aster_Mesh_Assistant
+{
+    class Code_Aster_Mesh_Assistant
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+        }
+    }
+}
