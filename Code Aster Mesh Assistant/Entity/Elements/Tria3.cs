30fb1f326fb7328a6178a7e9ddbf40a2e6a1d6dd
ibobey <ibrahim-tunc-02@hotmail.com>
2025-08-27 15:53:01 +0300

    Tria3 class created

---
+++ Code Aster Mesh Assistant/Entity/Elements/Tria3.cs
@@ -0,0 +1,49 @@
+﻿using Code_Aster_Mesh_Assistant.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Aster_Mesh_Assistant.Entity.Elements
+{
+    public class Tria3 : IPolygon
+    {
+        #region Fields
+        private int _Id;
+        private float _Area;
+        private float _ElementQuality;
+        private float _AngleQuality;
+        private List<INode> _Nodes = new();
+
+        private INode _N1;
+        private INode _N2;
+        private INode _N3;
+
+        #endregion
+
+        #region Properties
+        public int Id => this._Id;
+        public string Element => KeyWords.TRIA3;
+        public float Area { get => this._Area; set => this._Area = value; }
+        public float ElementQuality { get => this._ElementQuality; set => this._ElementQuality = value; }
+        public float AngleQuality { get => this._AngleQuality; set => this._AngleQuality = value; }
+        List<INode> IPolygon.Nodes => this._Nodes;
+
+        public INode N1 { get => this._N1; }
+        public INode N2 { get => this._N2; }
+        public INode N3 { get => this._N3; }
+
+        #endregion
+
+        #region Constructors
+        public Tria3(int Id_, INode N1_, INode N2_, INode N3_)
+        {
+            this._Id = Id_;
+            this._N1 = N1_;
+            this._N2 = N2_;
+            this._N3 = N3_;
+        }
+        #endregion
+    }
+}
