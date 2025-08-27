b097873775725fa2ac1a9c2ade1d0223e0940053
ibobey <ibrahim-tunc-02@hotmail.com>
2025-08-27 14:43:49 +0300

    File field/property and CheckFile methods inserted

--- Code Aster Mesh Assistant/Mesh/MeshReader.cs
+++ Code Aster Mesh Assistant/Mesh/MeshReader.cs
@@ -9,22 +9,27 @@ namespace Code_Aster_Mesh_Assistant.Mesh
     public class MeshReader
     {
         #region Fields
-
+        private string _FilePath;
         #endregion
 
         #region Properties
-
+        public string FilePath { get => this._FilePath; }
         #endregion
 
         #region Constructors
-        public MeshReader()
+        public MeshReader(string filePath)
         {
-            
+            this._FilePath = filePath;
+            IsFileExists();
         }
         #endregion
 
         #region Helper Methods
-
+        public bool IsFileExists()
+        {
+            if (File.Exists(this._FilePath)) return true;
+            throw new FileNotFoundException($"There is no file or file path {this._FilePath}");
+        }
         #endregion
 
         #region Reader
