f3888c2c85fc1a56c7dbef49125f704bb42b243f
ibobey <ibrahim-tunc-02@hotmail.com>
2025-08-27 16:22:09 +0300

    ILine interface and Seg2 class created

---
+++ Code Aster Mesh Assistant/Entity/Elements/Seg2.cs
@@ -0,0 +1,40 @@
+﻿using Code_Aster_Mesh_Assistant.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Aster_Mesh_Assistant.Entity.Elements
+{
+    public class Seg2
+    {
+        #region Fields
+        private int _Id;
+        private List<INode> _Nodes = new();
+
+        private INode _N1;
+        private INode _N2;
+
+        #endregion
+
+        #region Properties
+        public int Id => this._Id;
+        public string Element => KeyWords.SEG2;
+        List<INode> Nodes => this._Nodes;
+
+        public INode N1 { get => this._N1; }
+        public INode N2 { get => this._N2; }
+
+        #endregion
+
+        #region Constructors
+        public Seg2(int Id_, INode N1_, INode N2_)
+        {
+            this._Id = Id_;
+            this._N1 = N1_;
+            this._N2 = N2_;
+        }
+        #endregion
+    }
+}
---
+++ Code Aster Mesh Assistant/Entity/Interface/ILine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Aster_Mesh_Assistant.Entity.Interface
+{
+    public interface ILine
+    {
+        public int Id { get; }
+        public string Element { get; }
+        public List<INode> Nodes { get; }
+
+    }
+}
